95254cfbb0d008165ada221ca7596bcd0f06dda6
Omkar <naikomkar0252@gmail.com>
2025-07-19 19:30:27 +0530

    03_logging and exception handling

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ def get_requirements(file_path:str)->list[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements= [req.replace("\n", "") for req in requirements]
-
     if HYPHEN_E_DOT in requirements:
         requirements.remove(HYPHEN_E_DOT)
 
